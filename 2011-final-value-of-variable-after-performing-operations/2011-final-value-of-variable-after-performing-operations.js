d19f7b8ee83b3c9e038360c973be2cc31223ae14
abhinav anshul <contact.abhinavanshul@gmail.com>
2022-05-27 22:09:45 +0530

    Time: 95 ms (31.00%), Space: 42.3 MB (70.73%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+var finalValueAfterOperations = function(operations) {
+    let x = 0;
+    for (let i = 0; i < operations.length; i++) {
+        if (operations[i].includes("+")) {
+            x++;
+        } else {
+            x--;
+        }
+    }
+    return x;
+};
